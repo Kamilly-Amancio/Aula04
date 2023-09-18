4c8535a5ef721e1d00eccde15f46936ccc9de568
Kamilly Amancio <61789868+Kamilly-Amancio@users.noreply.github.com>
2023-09-17 22:25:37 -0300

    Add files via upload

---
+++ AULA04/Aula04.py
@@ -0,0 +1,68 @@
+#NOME: KAMILLY AMANCIO BATISTA 
+#MATRICULA: 202202570397
+#PARADIGMAS DE LINAGUEM DE PROGRAMÇAÕ EM PYTHON
+
+#AULA 04 - DIA: 04/09/2023
+
+
+"""
+print(type(1_000_000))
+x = 50.0
+y = 50,0
+print(x)
+print(y)
+"""
+
+"""
+
+print(type(2+3+1))
+print(type(2+3+1.0))
+"""
+
+"""
+print (2**3)
+print (type(2**3))
+print (type(2.0**3))
+"""
+
+"""
+x = 5/2
+print(x)
+
+"""
+
+"""
+z=10-20
+print(z)
+print(abs(z))
+"""
+
+"""
+curso = 'Ensino a Distância'
+print(curso)
+print(type(curso))
+print(curso.lower())
+print(curso.upper())
+print(curso.split())
+"""
+
+"""
+for i in range(2,9,3)
+	print(i)
+"""
+
+"""
+s = ['a, b']
+d = ["x", "a", "b", "c"]
+for x in s:
+print(x)
+print(d)
+print(len(s))
+print(s + d)
+print(3 * d)
+"""
+
+"""
+pessoas = {'454785987-25':['Ana','Maria'], '345765123-68':['Paulo','Souza']}
+print(pessoas['454785987-25'])
+"""
